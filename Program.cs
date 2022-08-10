1b43ea7057ce576d6fa63089050a50ede82aeb3d
serapcengiz <73667009+serapcengiz@users.noreply.github.com>
2022-08-10 15:36:17 +0300

    .netcore

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Solution {
+    static void Main(String[] args) {
+       
+        String inputString; 
+         inputString = Console.ReadLine(); 
+         Console.WriteLine("Hello, World.");
+        Console.WriteLine(inputString);
+
+        
+    }
+}
